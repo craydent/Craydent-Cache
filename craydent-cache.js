f89530f00a1bae69254e7a88add16a12c876ac24
clarkinada <clark.inada@hart.com>
2016-05-16 13:48:30 -0700

    0.0.1 release

---
+++ craydent-cache.js
@@ -0,0 +1,58 @@
+require('craydent');
+var fs = require('fs');
+var dir = '/cache/', curdir = '.' + dir;
+if (fs.existsSync(curdir)) {
+	fs.readdirSync(curdir).forEach(function (file) {
+		fs.unlinkSync(curdir+file);
+	});
+}
+module.exports = function(params){
+	var self = this;
+	params = params || {};
+	self.rootdir = dir;
+	this.refresh_interval = params.refresh_interval || 1000*60*60;
+	this.get = function (name){
+		return new Promise(function( res ){
+			fs.readFile(curdir + self.files[name].current,function(err,data){
+				res($c.tryEval(data,JSON.parse));
+			});
+		});
+	};
+	this.files = params.files || {};
+	this.add = function (name, func, refresh_interval){
+		return new Promise(function( res ) {
+			refresh_interval = refresh_interval || self.refresh_interval;
+			var files = self.files;
+			if (!func && /http(s?):\/\/.*/.test(name)) {
+				func = name;
+			}
+			var cb = function (data) {
+				if (!files[name]) {
+					files[name] = {};
+					files[name].cuid0 = files[name].cuid0 || $c.cuid();
+					files[name].cuid1 = files[name].cuid1 || $c.cuid();
+					files[name].current = files[name].cuid0;
+					files[name].refresh_interval = refresh_interval;
+					files[name].method = func;
+				}
+				var filename = files[name].current == files[name].cuid0 ? files[name].cuid1 : files[name].cuid0;
+				fs.writeFile(curdir + filename, typeof data == 'object' ? JSON.stringify(data) : data,function( err ) {
+					if (err) { res(false); }
+					files[name].current = filename;
+					res(true);
+					setTimeout(function () {
+						func(cb);
+					}, refresh_interval);
+				});
+			};
+			if ($c.isString(func)) {
+				func = function (callback) {
+					$c.ajax({url: name, onsuccess: callback});
+				}
+			}
+			$c.mkdirRecursive(self.rootdir,function(){
+				func(cb);
+			});
+		});
+	}
+};
